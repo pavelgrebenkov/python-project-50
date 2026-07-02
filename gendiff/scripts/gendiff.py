31e1befdc7832216ed0824e68e7012f0affdb9bd
pavel_grebenkov <pavellearnnow@gmail.com>
2026-07-02 18:26:20 +0400

    modified gendiff.py for entry point set-up

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from python-project-50!")
+import argparse
 
+
+def main():
+	parser = argparse.ArgumentParser(prog="gendiff", description="Compares two configuration files and shows a difference.")
+
+	parser.add_argument("first_file")
+	parser.add_argument("second_file")
+
+	args = parser.parse_args()
+
+	return args
 
 if __name__ == "__main__":
     main()
