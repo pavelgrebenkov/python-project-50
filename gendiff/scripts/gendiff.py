9454ee23a0949d4cb750eb422b9e0631134a280d
pavel_grebenkov <pavellearnnow@gmail.com>
2026-07-04 18:59:36 +0400

    modify gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,7 +6,7 @@ def main():
 
 	parser.add_argument("first_file")
 	parser.add_argument("second_file")
-	parser.add_argument("-f", "--format", help="set format of output", default="sylish")
+	parser.add_argument("-f", "--format", help="set format of output", default="stylish")
 
 	args = parser.parse_args()
 
