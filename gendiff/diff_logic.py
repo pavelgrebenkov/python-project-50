ec6b2b2f74c1924532d216e3bafff248794b3581
pavel_grebenkov <pavellearnnow@gmail.com>
2026-07-15 19:25:02 +0400

    docs: add docstrings and type hints to functions

--- gendiff/diff_logic.py
+++ gendiff/diff_logic.py
@@ -1,21 +1,49 @@
 import json
+from typing import Any
 
 
-def _read_file(file_path):
+def _read_file(file_path: str) -> dict:
+	"""
+	Read a JSON file and return its parsed contents as a Python object.
+
+	Args:
+		file_path (str): The path to the JSON file to be read.
+	Returns:
+		Python object (dict): The parsed JSON data.
+	"""
 	with open(file_path) as f:
 		return json.load(f)
 
 
-def _get_sorted_keys(dict1, dict2):
+def _get_sorted_keys(dict1: dict, dict2: dict) -> list:
+	"""
+	Extract dictionary keys and return a sorted list of unique keys.
+
+	Args:
+		dict1 (dict): dictionary 1
+		dict2 (dict): dictionary 2
+	Returns:
+		sorted_keys (list): a sorted list of the unique keys from dict1 and dict2
+	"""
 	sorted_keys = sorted(list(dict1.keys() | dict2.keys()))
 	return sorted_keys
 
 
-def _format_value(value):
+def _format_value(value: Any) -> str:
+	"""
+	Determine the data type of input values and convert them to strings.
+
+	Args:
+		value: Any Python data type object.
+	Returns:
+		string: All input values are converted to strings.
+	"""
 	match value:
 		case bool():
 			return str(value).lower()
 		case int():
+			return str(value)
+		case float():
 			return str(value)
 		case None:
 			return "null"
@@ -24,6 +52,17 @@ def _format_value(value):
 
 
 def generate_diff(file1_path: str, file2_path: str) -> str:
+	"""
+	Read two files, compare their contents and return a tree-like output.
+
+	Args:
+		file1_path (str): The path to the first file to be read.
+		file2_path (str): The path to the second file to be read.
+
+	Returns:
+		tree-like output (str): Changes are indicated with - (removed), + (added), and empty space (unchanged).
+
+	"""
 	dict1 = _read_file(file1_path)
 	dict2 = _read_file(file2_path)
 
