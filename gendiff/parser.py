c812c0ca73033ce01346f01afb8b32b31b8d1dcf
pavel_grebenkov <pavellearnnow@gmail.com>
2026-08-08 20:29:43 +0400

    refactor: modify logic of read_file() - the function now checks the format of input files - only JSON and YAML files are allowed - reads JSON and YAML files - throws ValueError if input file is unsupported

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,4 +1,4 @@
-"""Parsing module for reading and parsing files of different formats.
+"""Parser Module (parser.py) for reading and parsing files of different formats.
 
 This module detects files of .json or .yml / .yaml type, reads them and
 parses them into Python dictionaries. It also handles errors, if it detects
@@ -6,16 +6,30 @@ unsupported file formats.
 """
 
 import json
+from pathlib import Path
+
+import yaml
 
 
 def read_file(file_path: str) -> dict:
 	"""
-	Read a JSON file and return its parsed contents as a Python object.
+	Read a JSON or YAML file and return its parsed contents as a Python object.
 
 	Args:
-		file_path (str): The path to the JSON file to be read.
+		file_path (str): The path to the JSON or YAML file to be read.
 	Returns:
-		Python object (dict): The parsed JSON data is a dictionary.
+		Python object: The parsed JSON or YAML data is a dictionary.
+		Error: In case of an unsupported file format, raise ValueError.
 	"""
-	with open(file_path) as f:
-		return json.load(f)
+	# Get file extension
+	file_ext = Path(file_path).suffix.lower()
+
+	# Check if file ext is allowed
+	if file_ext not in ['.json', '.yaml', '.yml']:
+		raise ValueError(f"Unsupported file format {file_ext}. Only JSON and YAML allowed.")
+
+	# Check if file is JSON or YAML
+	with Path(file_path).open() as f:
+		if file_ext == ".json":
+			return json.load(f)
+		return yaml.safe_load(f)
