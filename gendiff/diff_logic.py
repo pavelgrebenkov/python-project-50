4e81c6e95bbae65cdb6add7da760320d85aad0fa
pavel_grebenkov <pavellearnnow@gmail.com>
2026-08-08 20:22:53 +0400

    refactor: added try-except block - try: Executes the main logic of comparing two input files of allowed format (JSON & YAML). - except: Catches ValueError from the parser and returns a user-friendly message in case one or both input files are unsupported.

--- gendiff/diff_logic.py
+++ gendiff/diff_logic.py
@@ -1,10 +1,10 @@
-""" Difference generator module for compring configuration files.
+""" Difference Generator Module (diff_logic.py) for compring configuration files.
 
-This module contains the function that compares two structured
+This module contains the function generate_diff() that compares two structured
 configuration files and reports their differences in a user-selected
 format.
 
-It also contains helper functions used by the main difference function.
+It also contains helper functions used by generate_diff().
 """
 
 
@@ -18,10 +18,10 @@ def _get_sorted_keys(dict1: dict, dict2: dict) -> list:
 	Extract dictionary keys and return a sorted list of unique keys.
 
 	Args:
-		dict1 (dict): dictionary 1
-		dict2 (dict): dictionary 2
+		dict1: dictionary 1
+		dict2: dictionary 2
 	Returns:
-		sorted_keys (list): a sorted list of the unique keys from dict1 and dict2
+		sorted_keys: a sorted list of the unique keys from dict1 and dict2
 	"""
 	sorted_keys = sorted(list(dict1.keys() | dict2.keys()))
 	return sorted_keys
@@ -49,38 +49,43 @@ def _format_value(value: Any) -> str:
 			return value
 
 
-def generate_diff(file1_path: str, file2_path: str) -> str:
+def generate_diff(file_path_1: str, file_path_2: str) -> str:
 	"""
 	Read two files, compare their contents and return a tree-like output.
 
 	Args:
-		file1_path (str): The path to the first file to be read.
-		file2_path (str): The path to the second file to be read.
+		file_path_1 (str): The path to the first file to be read.
+		file_path_2 (str): The path to the second file to be read.
 
 	Returns:
-		tree-like output (str): Changes are indicated with - (removed), + (added), and empty space (unchanged).
+		tree-like output (str): Changes are indicated with - (removed), + (added), and "  " (unchanged).
 
 	"""
-	dict1 = read_file(file1_path)
-	dict2 = read_file(file2_path)
+	try:
+		dict1 = read_file(file_path_1)
+		dict2 = read_file(file_path_2)
 
-	sorted_keys = _get_sorted_keys(dict1, dict2)
+		sorted_keys = _get_sorted_keys(dict1, dict2)
 
-	output_str = ""
+		output_str = ""
 
-	for key in sorted_keys:
+		for key in sorted_keys:
 
-		value1 = dict1.get(key)
-		value2 = dict2.get(key)
+			value1 = dict1.get(key)
+			value2 = dict2.get(key)
 
-		if key in dict1 and key in dict2:
-			if dict1.get(key) != dict2.get(key):
-				output_str += f"  - {key}: {_format_value(value1)}\n  + {key}: {_format_value(value2)}\n"
-			else:
-				output_str += f"    {key}: {_format_value(value1)}\n"
-		elif key in dict1:
-			output_str += f"  - {key}: {_format_value(value1)}\n"
-		elif key in dict2:
-			output_str += f"  + {key}: {_format_value(value2)}\n"
+			if key in dict1 and key in dict2:
+				if dict1.get(key) != dict2.get(key):
+					output_str += f"  - {key}: {_format_value(value1)}\n  + {key}: {_format_value(value2)}\n"
+				else:
+					output_str += f"    {key}: {_format_value(value1)}\n"
+			elif key in dict1:
+				output_str += f"  - {key}: {_format_value(value1)}\n"
+			elif key in dict2:
+				output_str += f"  + {key}: {_format_value(value2)}\n"
 
-	return f"{{\n{output_str}}}"
+		return f"{{\n{output_str}}}"
+
+	except ValueError as error:
+		# The ValueError from _read_file() propogates here
+		return f"Comparison failed: {error}"
