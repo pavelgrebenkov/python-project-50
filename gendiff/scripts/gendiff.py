a62c9319acfc5f21548ffc76abaad503e3628b4d
pavel_grebenkov <pavellearnnow@gmail.com>
2026-09-12 20:52:17 +0400

    Refactor CLI entry point.
    Add the format argument from args to generate_diff.

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -29,14 +29,14 @@ def main():
 	"""
 	parser = argparse.ArgumentParser(prog="gendiff", description="Compares two configuration files and shows a difference.")
 
-	parser.add_argument("first_file")
-	parser.add_argument("second_file")
+	parser.add_argument("first_file", help="Path to the first file")
+	parser.add_argument("second_file", help="Path to the second file")
 	parser.add_argument("-f", "--format", help="set format of output", default="stylish")
 
 	args = parser.parse_args()
 
 	try:
-		diff_string = generate_diff(args.first_file, args.second_file)
+		diff_string = generate_diff(args.first_file, args.second_file, args.format)
 		print(diff_string)
 
 	except ValueError as error:
